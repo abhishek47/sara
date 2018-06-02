0f789a0d87f8d3432af16c5db689d89e87a3f9fe
abhishek47 <operations@trumpetstechnologies.com>
2018-06-02 07:35:25 +0530

    alerts

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -24,6 +24,13 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+import Echo from "laravel-echo"
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'ddf6f87774360fdbabc6y'
+});
+
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
