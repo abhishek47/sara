f0d4e142a46d1ca1472ff1a5d0a6cc6a95d4505f
abhishek47 <operations@trumpetstechnologies.com>
2018-06-02 07:13:06 +0530

    changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -32,14 +32,3 @@ const app = new Vue({
     }
 });
 
-
-function flashy(message, link) {
-        var template = $($("#flashy-template").html());
-        $(".flashy").remove();
-        template.find(".flashy__body").html(message).attr("href", link || "#").end()
-         .appendTo("body").hide().fadeIn(300).delay(2800).animate({
-            marginRight: "-100%"
-        }, 300, "swing", function() {
-            $(this).remove();
-        });
-    }
--- resources/assets/js/components/UserNotifications.vue
+++ resources/assets/js/components/UserNotifications.vue
@@ -61,8 +61,22 @@
 
                      this.notifications.unshift(notificationElement);
 
+                     this.flashy(notification.message, notification.link);
+
 
                 });
-        }
+        },
+
+
+         flashy(message, link) {
+                var template = $($("#flashy-template").html());
+                $(".flashy").remove();
+                template.find(".flashy__body").html(message).attr("href", link || "#").end()
+                 .appendTo("body").hide().fadeIn(300).delay(2800).animate({
+                    marginRight: "-100%"
+                }, 300, "swing", function() {
+                    $(this).remove();
+                });
+            }
     }
 </script>
